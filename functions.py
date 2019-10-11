75994fb37d7923103bb67eb66a6ece971152267b
shri <you@example.com>
2019-10-11 11:35:19 +0530

    Updating functions

--- functions.py
+++ functions.py
@@ -25,9 +25,31 @@ def my_fev(food):
         kk = "{} is my frevrate fruits"  # We can also print like this
         print(kk.format(i))
 
+
 fruits = [ "mango", "apple", "banana", "cherry"]
 food =  fruits
 
 my_fev(food)
+
+
+# How to get number of arguments passed in funcation
+
+def my_funck(*kids):
+    KK = len(kids) - 1
+    print("The youngest child is " + kids[KK])
+    print(my_funck.__name__)
+
+
+my_funck("one", "two", "three", "four", "five")
+
+import sys
+
+def whoami():
+    return sys._getframe(1).f_code.co_name
+
+def func1():
+    print(whoami())
+
+func1()
 
 exit(10) # We can set exit code using exit() method
