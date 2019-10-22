3ea7eb6bb18c53e17b0f0fbdbaf9663911b87964
shri <you@example.com>
2019-10-22 11:59:34 +0530

    adding code in iterator

---
+++ iterator.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import time
+mytuple = ("shri", "kavi", "harsha")
+
+myit = iter(mytuple)
+
+print(next(myit))
+time.sleep(1)
+print(next(myit))
+time.sleep(1)
+print(next(myit))
+time.sleep(1)
+
+mystr = "shridhar"
+myitr = iter(mystr)
+
+
+print(next(myitr))
+print(next(myitr))
+print(next(myitr))
+print(next(myitr))
+
+
+
+for i in mytuple:
+    print(i)
+
+for x in mystr:
+    print(x)
+
+mystr = "Shridharkaviharsharamshri"
+
+myitr = iter(mystr)
+
+JK = len(mystr)
+
+for i in range(1, len(mystr)):
+    print(next(myitr))
+    time.sleep(0.1)
+
