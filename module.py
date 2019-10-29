649bdd5cdc50545da47216ec300583185481ac98
shri <you@example.com>
2019-10-29 22:44:02 +0530

    Update modules

---
+++ module.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+This is created to test module
+
+'''
+
+def mywish(name):
+    print("Happy birthday " + name)
+
+person1 = {
+    "Name" : "shri",
+    "Age" : 35,
+    "Country" : "India"
+}
+
