22bfc19926ac3c1b82b8218c1885f2ec1907a45f
shri <you@example.com>
2019-10-31 11:28:39 +0530

    Adding some code in test.json

--- test_json.py
+++ test_json.py
@@ -4,5 +4,13 @@ import json
 # Test data in jason format
 J = '{"name" : "shri", "age" : 35, "place" : "mumbai"}'
 
+# below part is dictionary
+K = {"name" : "shri", "age" : 35, "place" : "mumbai"}
+
 Y = json.loads(J)
+print(Y["name"])
 print(Y["age"])
+print(Y["place"])
+
+# how to use dictionary data 
+print(K["name"])
