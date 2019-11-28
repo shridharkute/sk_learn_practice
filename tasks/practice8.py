da6701ffd20909639531571db2b34f6013086821
shri <you@example.com>
2019-11-28 12:25:15 +0530

    Adding diff golabal and local var

--- tasks/practice8.py
+++ tasks/practice8.py
@@ -24,6 +24,6 @@ nu_prin()
 # Solution2
 for num in range(10):
     for i in range(num):
-        print (num, end=" ") #print number
+        print (num , end=" ") #print number
     # new line after each row to display pattern correctly
-    print("\n")
+    print("")
