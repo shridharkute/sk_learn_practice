4b91cb3a5310721e5c86bd29b590b9de776d841e
shri <you@example.com>
2019-12-05 11:17:56 +0530

    Added solution3 in practice 5

--- tasks/practice5.py
+++ tasks/practice5.py
@@ -9,7 +9,8 @@ This script will run only with single digit input
 
 '''
 
-# Solution 1
+# Solution 1 - Which is not working with double  digit
+# it will not working as expected with 12,1,2,3,4,5,12 input
 X = list(input("Please supply imput list: "))
 
 F = X[0]
@@ -23,7 +24,8 @@ else:
     print("result is false")
 
 
-# Solution 2
+# Solution 2 - Which is also not working with double  digit
+# it will not working as expected with 12,1,2,3,4,5,12 input
 Y = list(input("Please supply imput list: "))
 
 def F_and_L(numlist):
@@ -38,3 +40,18 @@ def F_and_L(numlist):
 
 print("The first and last number of the a list is same")
 print("Result is ", F_and_L(Y))
+
+
+# Solution 3 # it is updated solution1 # it should work with double digit
+# it will be working as expected with 12,1,2,3,4,5,12 input
+X = list(input("Please supply imput list: ").split(","))
+
+F = X[0]
+L = X[-1]
+
+if X[0] == X[-1]:
+    print("the first and last number of list is saame")
+    print("result is true")
+    print(f"First {F} Last {L}")
+else:
+    print("result is false")
