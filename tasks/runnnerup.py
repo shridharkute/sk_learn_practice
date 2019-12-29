423ede75edbd980907bc991aa000a343f19a07ca
shri <you@example.com>
2019-12-29 21:17:22 +0530

    adding soltuon to the runnebr up

--- tasks/runnnerup.py
+++ tasks/runnnerup.py
@@ -5,7 +5,7 @@ This script is created to find 2st runner up from the list of the results
 2 9 5 3 8 9 7
 '''
 
-
+# solution1
 if __name__ == '__main__':
     #n = int(input())
     n = 5
@@ -19,3 +19,25 @@ if __name__ == '__main__':
 
     print(f"{max(NEW)} is the runner up")
 
+
+# solution2
+
+if __name__ == '__main__':
+    #n = int(input())
+    n = 5
+    arr = map(int, input().split())
+    sk = list(arr)
+    for i in range(0,len(sk)):
+        max_pos = i
+        for k in range(i,len(sk)):
+            if sk[max_pos] < sk[k]:
+                max_pos = k
+
+        temp = sk[i]
+        sk[i] = sk[max_pos]
+        sk[max_pos] = temp
+
+for j in range(len(sk)):
+    if max(sk) != sk[j]:
+        print(sk[j])
+        break
